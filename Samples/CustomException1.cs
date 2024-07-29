90fb7a3657310e68e2cee055178001ee237f7b94
Lariyan.Pouya <Lariyan.Pouya@HIG001P4987>
2024-07-29 13:59:20 +0330

    Add project files.

---
+++ Samples/CustomException1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples
+{
+    internal class CustomException1 : Exception
+    {
+        public override string Message => "Exception 1";
+    }
+}
---
+++ Samples/Program.cs
@@ -0,0 +1,28 @@
+﻿using Samples;
+
+Method(0);
+Method(1);
+Method(2);
+
+Console.ReadKey();
+
+void Method(int a)
+{
+    try
+    {
+        switch (a)
+        {
+            case 0: throw new CustomException1();
+            case 1: throw new CustomException2();
+            case 2: throw new Exception("Unknow Exception");
+        }
+    }
+    catch (Exception exp) when (exp is CustomException1 || exp is CustomException2)
+    {
+        Console.WriteLine(exp.Message);
+    }
+    catch (Exception exp) 
+    {
+        Console.WriteLine(exp.Message);
+    }
+}
